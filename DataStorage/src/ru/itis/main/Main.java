fc238ac44e6c5435661254f985f7c990cf0bcce9
Aivar <aivaryusupov@gmail.com>
2017-04-19 22:30:57 +0300

    Update Main.java

--- DataStorage/src/ru/itis/main/Main.java
+++ DataStorage/src/ru/itis/main/Main.java
@@ -7,5 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
         UsersDataStorage storage = DataStorageFactory.getUsersDataStorage();
+        AutoDataStorage autosStorage = DataStorageFactory.getAutoDataStorage();
     }
 }
