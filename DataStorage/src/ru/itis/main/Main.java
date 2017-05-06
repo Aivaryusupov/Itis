a4a7e101f282014b4cc52f95eeb7a0aba7797397
Aivaryusupov <aivaryusupov@gmail.com>
2017-05-06 12:20:12 +0300

    changes

--- DataStorage/src/ru/itis/main/Main.java
+++ DataStorage/src/ru/itis/main/Main.java
@@ -4,19 +4,20 @@ import ru.itis.main.context.AppContext;
 import ru.itis.main.dao.*;
 import ru.itis.main.models.Auto;
 import ru.itis.main.models.User;
+import ru.itis.main.services.UsersService;
 //import ru.itis.main.models.User.Builder;
 
 public class Main {
 
     public static void main(String[] args) {
-        AppContext<User> appContext = new AppContext<>();
-        UsersDao usersDao = appContext.getComponent(UsersDao.class);
-        User myrad = new User.Builder().id(3).login("Vika").password("vikula").name("Виктория").age(22).build();
-        System.out.println(usersDao.findAllByName("Виктория"));
-
-
-
-
+        AppContext<Auto> appContext = new AppContext<>();
+        AppContext<User> appContext1 = new AppContext<>();
+        UsersDao usersDao = appContext1.getComponent(UsersDao.class);
+        AutoDao autoDao = appContext.getComponent(AutoDao.class);
+        UsersService usersService = appContext1.getComponent(UsersService.class);
+        User user = usersDao.find(2);
+        System.out.println(autoDao.findAllAutoByOwner(user));
+        System.out.println(usersService.isRegistered(user.getName()));
 
 
     }
